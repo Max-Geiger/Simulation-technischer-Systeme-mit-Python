09a2371c7718f8c51a2efa6bf22881fe1e30e38a
Max-Geiger <maxgeiger-geiger@web.de>
2023-03-08 08:58:33 +0100

    Klausur

---
+++ Klausur/Geiger_Max_4876959_Aufgabe1.py
@@ -0,0 +1,16 @@
+"""
+Author: Max Geiger
+Matrikelnummer: 4876959
+Datum: 08.03.2023
+Zweck: Klausur "Simulation technischer Systeme mit Python"
+"""
+#Imports:
+
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import pandas as pd
+#import scipy.optimize as op
+#from scipy.integrate import solve_ivp
+
+# Lösung der Aufgabe 1
+print('Hello World')
---
+++ Klausur/Geiger_Max_4876959_Aufgabe4.py
@@ -0,0 +1,15 @@
+"""
+Author: Max Geiger
+Matrikelnummer: 4876959
+Datum: 08.03.2023
+Zweck: Klausur "Simulation technischer Systeme mit Python"
+"""
+#Imports:
+
+#import numpy as np
+#import matplotlib.pyplot as plt
+#import pandas as pd
+#import scipy.optimize as op
+#from scipy.integrate import solve_ivp
+
+# Lösung der Aufgabe 4
